1eb779056a306c6d50dc1c8531dda310e87b4fb9
winter4 <krishna379nand@gmail.com>
2024-10-30 19:19:21 +0530

    carousel added in trailer section

--- src/components/Trailer/Trailer.jsx
+++ src/components/Trailer/Trailer.jsx
@@ -1,21 +1,53 @@
-import React from "react";
-import "./style.scss";
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
-const Trailer = ({dataId}) => {
-  const{mediaType}=useParams()
-  const {data,loading} = useFetch(`/${mediaType}/${dataId}/videos`)
+import { Skeleton } from "@mui/material";
+import "./style.scss";
 
-  
-  const source = `https://www.youtube.com/embed/${data?.results[0]?.key}`;
+const Trailer = ({ dataId }) => {
+  const { mediaType } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${dataId}/videos`);
+  const [index, setIndex] = useState(0);
+
+  // Handle carousel index change
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  // Filter for YouTube videos if they exist
+  const videos = data?.results?.filter(video => video.site === "YouTube");
 
   return (
-    <div className="Trailer">
-      
-      <iframe src={source}  width="1200px"
-        height= "750px" allowfullscreen="true"></iframe>
+    <div className="trailer-carousel-container">
+      {loading ? (
+        <Skeleton variant="rounded" width="100%" height="750px" />
+      ) : videos?.length ? (
+        <Carousel
+          activeIndex={index}
+          onSelect={handleSelect}
+          style={{ width: "1200px", height: "750px" }}
+        >
+          {videos.map((video, idx) => (
+            <Carousel.Item key={idx}>
+              <iframe
+                src={`https://www.youtube.com/embed/${video.key}`}
+                width="100%"
+                height="750px"
+                title={video.name}
+                allowFullScreen
+                style={{
+                  borderRadius: "10px",
+                  overflow: "hidden",
+                }}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      ) : (
+        <p>No trailers available</p>
+      )}
     </div>
-    
   );
 };
 
